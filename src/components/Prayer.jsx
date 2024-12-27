cc08ed0ea129bd1e3ad30a3d9ddf05550baaf1dc
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2024-12-27 04:57:30 +0200

    first commit

--- src/components/Prayer.jsx
+++ src/components/Prayer.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 export default function Prayer({ name, time }) {
   return (
-    <div id="prayer" className="flex justify-between px-6 py-3 rounded-md m-4">
+    <div id="prayer" className="flex justify-between px-6 py-3 rounded-md my-4">
       <p className=" text-2xl">{`${name} :`}</p>
       <p className=" text-2xl">{time}</p>
     </div>
