294648054e13d8ccc558a7feb744b2ee96d9f0cb
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2024-12-28 07:04:04 +0200

    `Removed trailing colon from prayer name in Prayer component`

--- src/components/Prayer.jsx
+++ src/components/Prayer.jsx
@@ -2,7 +2,7 @@
 export default function Prayer({ name, time }) {
   return (
     <div id="prayer" className="flex justify-between px-6 py-3 rounded-md my-4">
-      <p className=" text-2xl">{`${name} :`}</p>
+      <p className=" text-2xl">{`${name} `}</p>
       <p className=" text-2xl">{time}</p>
     </div>
   );
