ecef4ca28d4af326a6d8c600a5319419230b033f
Ahmed Mustafa Kamel <101ahmedmustafa@gmail.com>
2024-12-28 05:34:11 +0200

    `Update date format in App component to use slashes instead of hyphens`

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,8 @@ function App() {
             <div className="date space-y-2">
               <h3 className=" text-gray-200">{GregianDate}</h3>
               <h3 className=" text-gray-200">
-                {parseInt(HijriDateDay) + 1}-{HijriDateMonth}-{HijriDateYear}هـ
+                {parseInt(HijriDateDay) + 1}/ {HijriDateMonth}/ {HijriDateYear}
+                هـ
               </h3>
             </div>
           </div>
